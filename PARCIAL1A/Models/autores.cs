acc46b480246665dbe378c181a289805cddcdcf5
jserr <jserr@JC>
2024-03-09 12:47:56 -0600

    se actualizo autores

--- PARCIAL1A/Models/autores.cs
+++ PARCIAL1A/Models/autores.cs
@@ -2,5 +2,9 @@
 {
     public class autores
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        // Otras propiedades del autor
+        public ICollection<AutoresLibro> AutoresLibros { get; set; }
     }
 }
